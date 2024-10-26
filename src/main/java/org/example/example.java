d2de16babc2c56c956022e1ae7307572819b90ae
EgorovaOV <ove2501@mail.ru>
2024-10-26 11:09:00 +0500

    открыли страницу и сделали скрин

---
+++ src/main/java/org/example/example.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+
+public class example {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+            System.out.println(page.title());
+
+        }
+    }
+}
