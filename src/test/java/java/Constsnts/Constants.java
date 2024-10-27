e7dc66c89292d31b649837bba0ae926cc4998ed1
EgorovaOV <ove2501@mail.ru>
2024-10-27 11:16:44 +0500

    добавили константы в файл для бьюти

---
+++ src/test/java/java/Constsnts/Constants.java
@@ -0,0 +1,19 @@
+package java.Constsnts;
+
+public class Constants {
+    //переменная, метод или класс не могут быть изменены после их инициализации
+    public static final String baseUrl = "https://beauty-priority.ru/";
+    public static final String selectorForSliderMainPage = "t-slds__items-wrapper t-slds_animated-none";
+    public static final String selectorForEvents = "#rec578985895";
+    public static final String selectorForActions = "#rec570710170";
+    public static final String selectorForOurServices = "t-section__title t-title t-title_xs t-align_left";
+    public static final String selectorForOurTeam = "#rec247122420";
+    public  static final String selectorForTeamFoto = "t603";
+    public static final String selectorForWorksFoto = "#rec365143582";
+    public static final String selectorAboutUs = "#rec247126236";
+    public static final String selectorForYourQuestion = "#rec162574512";
+    public static final String selectorForClientsFeedback = "#rec121302375";
+    public static final String selectorForFooter = "#t-footer";
+    public static final String selectorbuttonForOnline = "body > a.yButton.bottom.right > div.yButtonWave";
+
+}
